81bd7391e97374d40dc4fd4d4a06c3b7915af558
zxeenu <zeenu.lastorder@gmail.com>
2024-09-18 00:21:31 +0500

    Fixed FART

--- app/root.tsx
+++ app/root.tsx
@@ -146,7 +146,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body>
         <MantineProvider
