890320b687b70f59b4ce5c38830e932eba8738b0
Zxeenu <zeenu.lastorder@gmail.com>
2024-10-10 18:43:56 +0500

    Changes to web server config

--- app/lib/session.server.ts
+++ app/lib/session.server.ts
@@ -1,6 +1,8 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 import { env } from "~/lib/env.server";
 
+const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
+
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -9,8 +11,9 @@ export let sessionStorage = createCookieSessionStorage({
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
     secrets: [env("SESSION_SECRET")], // replace this with an actual secret
-    // secure: process.env.NODE_ENV === "production", // enable this in prod only
-    secure: false, // enable this in prod only
+    secure: process.env.NODE_ENV === "production", // enable this in prod only
+    // secure: false, // enable this in prod only
+    maxAge: ONE_WEEK_IN_SECONDS,
   },
 });
 
